e13b6c59ae5d162d29097df3ba2a3e50b5d8d65f
Pedro Pastuk <pastukpedro@gmail.com>
2021-10-11 16:14:11 -0300

    project setup complete

--- Typescript/trilhaNodejs/src/controller/MainController.ts
+++ Typescript/trilhaNodejs/src/controller/MainController.ts
@@ -1,10 +1,14 @@
 import { Request, Response, NextFunction } from 'express';
 export default {
-  async logRoutes(req: Request, res: Response, next: NextFunction) {
+  async logRoutes(
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ): Promise<void> {
     console.log(`[${req.method.toUpperCase()}] -> ${req.url}`);
     return next();
   },
-  async index(req: Request, res: Response) {
+  async index(req: Request, res: Response): Promise<Response> {
     return res.json({
       message: 'Hello World!',
     });
