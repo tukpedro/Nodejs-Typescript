da608dd7cb4e5dcb623604819a246dfbaec45133
Pedro Pastuk <pastukpedro@gmail.com>
2021-10-18 13:29:48 -0300

    create customers routes

--- api-restful-sales/src/shared/http/routes/index.routes.ts
+++ api-restful-sales/src/shared/http/routes/index.routes.ts
@@ -4,6 +4,7 @@ import usersRouter from '@modules/users/routes/users.routes';
 import sessionsRouter from '@modules/users/routes/sessions.routes';
 import isAuthenticated from '../middlewares/isAuthenticated';
 import passwordRouter from '@modules/users/routes/password.routes';
+import customersRouter from '@modules/customers/routes/customers.routes';
 
 const routes = Router();
 
@@ -12,5 +13,6 @@ routes.use('/password', passwordRouter);
 routes.use(isAuthenticated);
 routes.use('/products', productsRouter);
 routes.use('/users', usersRouter);
+routes.use('/customers', customersRouter);
 
 export default routes;
