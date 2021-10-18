c28dc86f3144b84438fefcfa84dda92a9aeb8f78
Pedro Pastuk <pastukpedro@gmail.com>
2021-10-18 17:25:54 -0300

    creating migration of orders

---
+++ api-restful-sales/src/shared/typeorm/migrations/1634588703684-CreateOrders.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateOrders1634588703684 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
