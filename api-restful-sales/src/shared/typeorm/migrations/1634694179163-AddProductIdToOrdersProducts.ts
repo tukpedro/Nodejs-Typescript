8499926fe51fb843d7f6ac08a6c7a605bc0204df
Pedro <pastukpedro@gmail.com>
2021-10-19 23:02:14 -0300

    create foreignkeys: product_id, order_id

--- api-restful-sales/src/shared/typeorm/migrations/1634694179163-AddProductIdToOrdersProducts.ts
+++ api-restful-sales/src/shared/typeorm/migrations/1634694179163-AddProductIdToOrdersProducts.ts
@@ -5,23 +5,24 @@ import {
   TableForeignKey,
 } from 'typeorm';
 
-export class AddCustomerIdToOrders1634663901407 implements MigrationInterface {
+export class AddProductIdToOrdersProducts1634694179163
+  implements MigrationInterface
+{
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.addColumn(
-      'orders',
+      'orders_products',
       new TableColumn({
-        name: 'customer_id',
+        name: 'product_id',
         type: 'uuid',
         isNullable: true,
       }),
     );
-
     await queryRunner.createForeignKey(
-      'orders',
+      'orders_products',
       new TableForeignKey({
-        name: 'OrdersCustomer',
-        columnNames: ['customer_id'],
-        referencedTableName: 'customers',
+        name: 'OrdersProductsProduct',
+        columnNames: ['product_id'],
+        referencedTableName: 'products',
         referencedColumnNames: ['id'],
         onDelete: 'SET NULL',
       }),
@@ -29,7 +30,10 @@ export class AddCustomerIdToOrders1634663901407 implements MigrationInterface {
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropForeignKey('orders', 'OrdersCustomer');
-    await queryRunner.dropColumn('orders', 'customer_id');
+    await queryRunner.dropForeignKey(
+      'orders_products',
+      'OrdersProductsProduct',
+    );
+    await queryRunner.dropColumn('orders_products', 'product_id');
   }
 }
