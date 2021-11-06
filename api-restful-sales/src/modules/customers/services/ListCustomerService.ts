12f22a4bb127662c013e21a6e9bc42c5c5d76f38
Pedro Pastuk <pedro.pastuk@alexandria.solar>
2021-11-06 14:33:39 -0300

    bug fix

--- api-restful-sales/src/modules/customers/services/ListCustomerService.ts
+++ api-restful-sales/src/modules/customers/services/ListCustomerService.ts
@@ -1,19 +1,3 @@
-// import { getCustomRepository } from 'typeorm';
-// import Customer from '../typeorm/entities/Customer';
-// import CustomersRepository from '../typeorm/repositories/CustomersRepository';
-
-// class ListCustomerService {
-//   public async execute(): Promise<Customer[]> {
-//     const customersRepository = getCustomRepository(CustomersRepository);
-
-//     const customers = customersRepository.find();
-
-//     return customers;
-//   }
-// }
-
-// export default ListCustomerService;
-
 import { getCustomRepository } from 'typeorm';
 
 import Customer from '../typeorm/entities/Customer';
