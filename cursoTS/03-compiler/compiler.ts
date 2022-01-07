6b63754186d59d2d5656608a7b3d31800e384376
Pedro Pastuk Pontes <pedrop@vegait.com.br>
2022-01-07 18:08:57 -0300

    [#005] esmascript

--- cursoTS/03-compiler/compiler.ts
+++ cursoTS/03-compiler/compiler.ts
@@ -1,9 +1,14 @@
 const channel: string = 'NerdBTC'
 const subscriptions: number = 1000000
 
-// channel = subscriptions; -> gera erro, porém com o 'noEmitOnError' marcado como 'true', o tsc nao vai gerar o arquivo js
+// channel = subscriptions; -> gera ERRO, porém com o 'noEmitOnError' marcado como 'true', o tsc nao vai gerar o arquivo js
 
-// let myName = 'pedro' -> gera erro pq foi declarado em outro arquivo e não foi setado para usar modulos.
+// let myName = 'pedro' -> gera ERRO pq foi declarado em outro arquivo e não foi setado para usar modulos.
 
 // "sourceMap": true -> gera o arquivo .js.map que serve para o browser mapear o arquivo .ts
 
+// function sum(a, b) { return a + b } -> gera ERRO pq o tsc está setado "noImplicitAny": true, ou seja, vc tem que declarar o tipo dela.
+
+// "strictNullChecks": true -> verifica se uma variavel pode vir null
+
+// 
